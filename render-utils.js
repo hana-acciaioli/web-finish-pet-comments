2a93cfdf7f8c253a6a043bab6067ca55c4e281b9
Hana Acciaioli <hana.acciaioli@gmail.com>
2022-10-05 12:01:13 -0700

    Pet items link to pet detail page

--- render-utils.js
+++ render-utils.js
@@ -2,7 +2,9 @@ export function renderPet(pet) {
     const li = document.createElement('li');
 
     // Part A: add an anchor link tag
+    const a = document.createElement('a');
     //      - set the link to be like `/pet/?id=34`, but use the actual pet id
+    a.href = `/pet/?id=${pet.id}`;
     //      - adjust the content to be in the anchor link,
     //        and the anchor link is appended to the li
 
@@ -16,6 +18,7 @@ export function renderPet(pet) {
     p.textContent = pet.bio;
 
     li.append(img, h2, p);
+    li.append(a);
 
     return li;
 }
